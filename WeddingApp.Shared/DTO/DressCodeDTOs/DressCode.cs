f5e42bd4beb424ecb71da0cdb17ffab2ad9caaff
matthew-gernale <dev.mattgernale@gmail.com>
2026-08-16 00:14:30 +0800

    fix: UI enhancement - [x] Updated font usage for the titles for better readability. - [x] Updated naming for the nav for easy comprehension. - [x] fixed the footer section.
    feat: Dress code section initial content
    - [x] Added DTO for the dress code.
    - [x] Added filter buttons for dress code.
    - [x] Added layout for dynamic preview of dress code.
    - [x] Added dress code instance for bride and groom.

---
+++ WeddingApp.Shared/DTO/DressCodeDTOs/DressCode.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WeddingApp.Shared.DTO.DressCodeDTOs
+{
+    public class DressCode
+    {
+        public Roles Role { get; set; }
+        public List<string>? ColorPalette { get; set; }
+        public List<string>? Attires { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ WeddingApp.Shared/Enum/Roles.cs
@@ -0,0 +1,16 @@
+﻿
+namespace WeddingApp.Shared.Enum
+{
+    public enum Roles
+    {
+        BRIDE,
+        GROOM,
+        FATHER,
+        MOTHER,
+        GROOMSMEN,
+        BRIDESMAID,
+        GODFATHER,
+        GODMOTHER,
+        GUEST
+    }
+}
