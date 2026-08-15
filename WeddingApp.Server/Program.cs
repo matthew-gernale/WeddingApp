82c0535bc07600b693b553ba5d7ac3aebd68c62e
matthew-gernale <dev.mattgernale@gmail.com>
2026-08-15 15:18:38 +0800

    added webassembly registration in program.cs

--- WeddingApp.Server/Program.cs
+++ WeddingApp.Server/Program.cs
@@ -19,12 +19,14 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
 app.MapControllers();
-
 app.MapFallbackToFile("index.html");
 
 app.Run();
