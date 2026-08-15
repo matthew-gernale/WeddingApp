4f26ed9e29f21c45bb45cc15fef16aad8d9b6a0a
matthew-gernale <dev.mattgernale@gmail.com>
2026-08-15 15:54:18 +0800

    Registered razor pages in program.cs

--- WeddingApp.Server/Program.cs
+++ WeddingApp.Server/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddHttpClient<IGDriveRepository, GDriveRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
