66bd696170156cf97e84f1ea97de32d5ced617ce
matthew-gernale <dev.mattgernale@gmail.com>
2026-08-26 17:53:18 +0800

    feat: Implement timeline page - [x] Added Timeline component. - [x] Added TimelineItem component. - [x] Added TimelineItem DTO. - [x] Added TimelineItem data. - [x] Added timeline page content and layout.
    fix: Fixed content grammar and spelling.

---
+++ WeddingApp.Shared/DTO/TimelineDTOs/TimelineItemDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace WeddingApp.Shared.DTO.TimelineDTOs
+{
+    public class TimelineItemDTO
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime Time { get; set; } = DateTime.UtcNow;
+        public string Icon { get; set; } = string.Empty;
+    }
+}
