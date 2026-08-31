b71fa8284ff34aa53cc85708368407cbc3669784
matthew-gernale <dev.mattgernale@gmail.com>
2026-08-31 21:27:21 +0800

    feat: Implemented Entourage page - [x] Added entourage hero page. - [x] Added entourage page content and layout. - [x] Added entourage data.

---
+++ WeddingApp.Shared/DTO/GuestDTOs/GuestDTO.cs
@@ -0,0 +1,12 @@
+﻿
+namespace WeddingApp.Shared.DTO.GuestDTOs
+{
+    public class GuestDTO
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int TableNumber { get; set; }
+
+        public Roles Role { get; set; }
+    }
+}
---
+++ WeddingApp.Shared/DTO/GuestDTOs/WeddingTableDTO.cs
@@ -0,0 +1,14 @@
+﻿
+namespace WeddingApp.Shared.DTO.GuestDTOs
+{
+    public class WeddingTableDTO
+    {
+        public int TableNumber { get; set; }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public int SeatCount { get; set; } = 8;
+    }
+}
